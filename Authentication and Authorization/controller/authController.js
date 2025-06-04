1fee604de0f82c1c6481a0084964ba54903eb598
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-06-04 12:19:30 +0530

    feat: Add authentication and authorization routes and views
    - Implemented authRoute for login and logout functionalities.
    - Created favouriteRoute for managing favorite houses.
    - Developed homeRoute for displaying and adding homes.
    - Added hostRoute for editing and deleting homes.
    - Introduced pathUtils for path management.
    - Designed 404 error page and various views for home, add home, and favorites.
    - Created forms for user login and house management.
    - Enhanced navigation with dynamic tab highlighting based on user state.

--- Authentication and Authorization/controller/authController.js
+++ Authentication and Authorization/controller/authController.js
@@ -1,10 +1,19 @@
 exports.getLogin = (req, res, next) => {
   res.render('auth/login', { isLoggedIn: false });
 }
+exports.signup = (req, res, next) => {
+  res.render('auth/signup', { isLoggedIn: false });
+}
 
 exports.postLogin = (req, res, next) => {
   req.session.isLoggedIn = true;
   res.redirect('/');
+}
+
+exports.postSignup = (req, res, next) => {
+  console.log(req)
+  console.log(req.body);
+  res.redirect('/login');
 }
 
 exports.logout = (req, res, next) => {
--- Authentication and Authorization/routes/authRoute.js
+++ Authentication and Authorization/routes/authRoute.js
@@ -8,5 +8,7 @@ const authController = require('../controller/authController');
 authRoute.get('/login', authController.getLogin);
 authRoute.post('/login', authController.postLogin);
 authRoute.get('/logout', authController.logout);
+authRoute.get('/signup', authController.signup);
+authRoute.post('/signup', authController.postSignup);
 
 exports.authRoute = authRoute;
