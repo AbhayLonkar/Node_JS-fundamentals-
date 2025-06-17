3280c8666c4935a103369e84447eec82644ace89
Abhay Lonkar <abhaylonkar9@gmail.com>
2025-06-17 18:11:33 +0530

    add functions to call api for deleting, updating and adding todoItems

--- 15.todo_app_with_backend/frontend/services/itemsService.js
+++ 15.todo_app_with_backend/frontend/services/itemsService.js
@@ -10,6 +10,28 @@ export const addItemToServer = async (task, date) => {
   return mapServerItemsToLocalItems(item);
 }
 
+export const getItemFromServer = async () => {
+  const response = await fetch('http://localhost:300/api/');
+  const items = await response.json();
+  return mapServerItemsToLocalItems(items);
+}
+
+export const markItemCompleted = async (id) => {
+  const response = await fetch(`http://localhost:3000/api/${id}/completed`, {
+    method: "PUT",
+  });
+  const item = await response.json();
+  return mapServerItemsToLocalItems(item);
+}
+
+export const deleteItem = async (id) => {
+  const response = await fetch(`http://localhost:3000/${id}`, {
+    method: "DELETE",
+  });
+  const item = response.json();
+  return item._id;
+}
+
 const mapServerItemsToLocalItems = (item) => {
   return {
     id: item._id,
